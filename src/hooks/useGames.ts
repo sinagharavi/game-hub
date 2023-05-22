9b14445dcc79729d1081cac246daabf129c0d395
Sina Gharavi <sina.gharavi@compliancemetrix.com>
2023-05-21 22:08:25 -0700

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   rating: number;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
