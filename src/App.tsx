92295f944bb82c8f8113e497c97ccc9f0cd4c684
Sina Gharavi <sina.gharavi@compliancemetrix.com>
2023-05-30 09:32:41 -0700

    Handle games with no image

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
-  sortOrder: String;
+  sortOrder: string;
 }
 
 function App() {
