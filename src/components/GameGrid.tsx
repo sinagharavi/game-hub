ec26d56e96ec40a9888cff320e86b09300f32f2e
Sina Gharavi <sina.gharavi@compliancemetrix.com>
2023-10-15 17:36:52 -0700

    Cleanup the gamecards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,11 +10,15 @@ interface Props {
 
 const GameGrid = ({ gemeQuery }: Props) => {
   const { data, error, isLoading } = useGames(gemeQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text color="red">{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={3}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
