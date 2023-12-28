d7d9b4c76f09a1c1e8e450a8e5a3ab495f31452e
Sina Gharavi <sina.gharavi@compliancemetrix.com>
2023-12-28 10:18:08 -0800

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,10 +8,12 @@ const apiClient = new APICLient<Game>("/games");
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  description_raw: string;
 }
 
 const useGames = () => {
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,13 @@
+import { Box } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <>
+    <Box padding={5}>
       <NavBar />
       <Outlet />
-    </>
+    </Box>
   );
 };
 
