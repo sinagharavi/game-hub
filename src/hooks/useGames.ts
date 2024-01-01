2d9643a75b4217d4ba06b03b6b2b254786fb5c1e
Sina Gharavi <sina.gharavi@compliancemetrix.com>
2024-01-01 11:01:41 -0800

    Refactor: moved entity enterphases

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,20 +2,9 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APICLient, { FetchResponse } from "../services/api-client";
 import useGameQueryStore from "../store";
-import { Platform } from "./usePlatforms";
+import { Game } from "../entities/Game";
 
 const apiClient = new APICLient<Game>("/games");
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-  description_raw: string;
-}
-
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
   return useInfiniteQuery<FetchResponse<Game>, Error>({
